59a5da541dd876bae704136de0aeec2b21102ea3
Yahoo <sjffhdfg@gmail.com>
2026-05-04 19:32:55 +0900

    Add.CameraSensorComponent

---
+++ Source/YeongCarCar/Camera/CameraSensorComponent.h
@@ -0,0 +1,124 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "Camera/CameraType.h"
+#include "CameraSensorComponent.generated.h"
+
+
+
+class USceneCaptureComponent2D;
+class UTextureRenderTarget2D;	
+UCLASS(ClassGroup = (Sensor), meta = (BlueprintSpawnableComponent), BlueprintType)
+
+
+
+class YEONGCARCAR_API UCameraSensorComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:
+	UCameraSensorComponent();
+
+	UTextureRenderTarget2D* GetRednerTarget() const { return RenderTarget; }
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void OnRegister() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	
+	
+private:
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+	
+	UFUNCTION(BlueprintCallable, Category = "CameraSensor")
+	void ApplyPreset(ECameraSensorPreset NewPreset);
+	
+	UFUNCTION(BlueprintCallable, Category = "CameraSensor")
+	void RefreshSettings();
+	
+	UFUNCTION(BlueprintCallable, Category = "CameraSensor")
+	void CaptureOnce();
+	
+	UFUNCTION(BlueprintCallable, Category = "CameraSensor")
+	void SetCaptureRate(float Hz);
+	
+	void InitializeCapture();
+	void ConfigureSceneCapture();
+	void CreateRenderTarget();
+	void StopCaptureTimer();
+	void ApplyPostProcessSettings();
+	void ApplyLensDistortion();
+	void StartCaptureTimer();
+	void OnCaptureTimer();
+	
+	void SaveCameraImage();
+	
+	
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CameraSensor|Output",
+	meta=(AllowPrivateAccess="true"))
+	TObjectPtr<UTextureRenderTarget2D> RenderTarget;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CameraSensor|Config",
+	meta=(AllowPrivateAccess="true"))
+	ECameraSensorPreset Preset = ECameraSensorPreset::TeslaHW3_Wide;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CameraSensor|Intrinsics",
+	meta=(AllowPrivateAccess="true"))
+	FCameraSensorIntrinsics Intrinsics;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CameraSensor|Distortion",
+		meta=(AllowPrivateAccess="true"))
+	FLensDistortionParams Distortion;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CameraSensor|Noise",
+		meta=(AllowPrivateAccess="true"))
+	FSensorNoiseParams Noise;
+
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CameraSensor|PostProcess",
+		meta=(AllowPrivateAccess="true"))
+	FCameraPostProcessEffects PostProcess;
+
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CameraSensor|Exposure",
+		meta=(AllowPrivateAccess="true"))
+	FAutoExposureParams Exposure;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CameraSensor|Config",
+	meta=(AllowPrivateAccess="true"))
+	bool bSensorEnabled = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CameraSensor|DataSave",
+		meta=(AllowPrivateAccess="true"))
+	bool bIsDataSaving = false;
+	
+	//이미지를 저장할 때 필요한 정보들을 담고 있는 구조체
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CameraSensor|DataSave",
+	meta=(AllowPrivateAccess="true"))
+	FSensorDataSaveConfig DataSaveConfig;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CameraSensor|Output",
+	meta=(AllowPrivateAccess="true"))
+	int64 FrameCount;
+	//모든 타입의 머테리얼을 담을수있는 상자
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CameraSensor|Distortion",
+		meta=(AllowPrivateAccess="true"))
+	TObjectPtr<UMaterialInterface> LensDistortionMaterial;
+	// 동적 머테리얼 인스턴스를 담기위한 상자
+	UPROPERTY()
+	TObjectPtr<UMaterialInstanceDynamic> DistortionMID;
+
+	
+	// 이미지 생성용 카메라 
+	UPROPERTY()
+	TObjectPtr<USceneCaptureComponent2D> SceneCapture;
+	
+	FTimerHandle CaptureTimerHandle;
+};
---
+++ Source/YeongCarCar/Camera/CameraType.h
@@ -0,0 +1,131 @@
+UENUM(BlueprintType)
+enum class ECameraSensorPreset : uint8
+{
+	Custom           UMETA(DisplayName = "Custom"),
+	TeslaHW3_Wide    UMETA(DisplayName = "Tesla HW3 Wide (120°)"),
+	TeslaHW3_Main    UMETA(DisplayName = "Tesla HW3 Main (50°)"),
+	TeslaHW3_Narrow  UMETA(DisplayName = "Tesla HW3 Narrow (35°)"),
+	TeslaHW4         UMETA(DisplayName = "Tesla HW4 (5MP)"),
+	DroneFPV         UMETA(DisplayName = "Drone FPV (150°)"),
+	Waymo            UMETA(DisplayName = "Waymo 5th Gen")
+};
+
+USTRUCT(BlueprintType)
+struct FCameraSensorIntrinsics
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Intrinsics", meta = (ClampMin = "1", ClampMax = "7680"))
+	int32 ImageWidth = 1280;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Intrinsics", meta = (ClampMin = "1", ClampMax = "4320"))
+	int32 ImageHeight = 960;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Intrinsics", meta = (ClampMin = "1.0", ClampMax = "120.0"))
+	float FrameRate = 36.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Intrinsics", meta = (ClampMin = "1.0", ClampMax = "180.0"))
+	float FOVDegrees = 120.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Intrinsics", meta = (ClampMin = "0.1"))
+	float FocalLengthMM = 1.5f;
+};
+
+USTRUCT(BlueprintType)
+struct FLensDistortionParams
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Distortion")
+	float K1 = -0.3f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Distortion")
+	float K2 = 0.1f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Distortion")
+	float K3 = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Distortion")
+	float P1 = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Distortion")
+	float P2 = 0.0f;
+
+	bool HasDistortion() const
+	{
+		return !FMath::IsNearlyZero(K1) || !FMath::IsNearlyZero(K2) || !FMath::IsNearlyZero(K3)
+			|| !FMath::IsNearlyZero(P1) || !FMath::IsNearlyZero(P2);
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FSensorNoiseParams
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Noise")
+	bool bEnableNoise = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Noise", meta = (EditCondition = "bEnableNoise"))
+	float GaussianMean = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Noise", meta = (EditCondition = "bEnableNoise", ClampMin = "0.0", ClampMax = "0.5"))
+	float GaussianStdDev = 0.02f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Noise", meta = (EditCondition = "bEnableNoise", ClampMin = "0.0", ClampMax = "1.0"))
+	float ShotNoiseIntensity = 0.01f;
+};
+
+USTRUCT(BlueprintType)
+struct FCameraPostProcessEffects
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PostProcess", meta = (ClampMin = "0.0", ClampMax = "2.0"))
+	float VignetteIntensity = 0.4f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PostProcess", meta = (ClampMin = "0.0", ClampMax = "5.0"))
+	float LensFlareIntensity = 0.1f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PostProcess", meta = (ClampMin = "0.0", ClampMax = "5.0"))
+	float ChromaticAberration = 0.5f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PostProcess", meta = (ClampMin = "0.0", ClampMax = "1.0"))
+	float MotionBlurAmount = 0.5f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PostProcess", meta = (ClampMin = "0.0", ClampMax = "5.0"))
+	float BloomIntensity = 0.0f;
+};
+
+USTRUCT(BlueprintType)
+struct FSensorDataSaveConfig
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DataSave")
+	FString SensorLabel = TEXT("Sensor");
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DataSave", meta = (ClampMin = "1", ClampMax = "100"))
+	int32 JPGQuality = 95;
+};
+
+USTRUCT(BlueprintType)
+struct FAutoExposureParams
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Exposure")
+	bool bEnableAutoExposure = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Exposure", meta = (EditCondition = "bEnableAutoExposure"))
+	float MinEV = 2.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Exposure", meta = (EditCondition = "bEnableAutoExposure"))
+	float MaxEV = 14.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Exposure", meta = (EditCondition = "bEnableAutoExposure", ClampMin = "0.1", ClampMax = "20.0"))
+	float SpeedUp = 3.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Exposure", meta = (EditCondition = "bEnableAutoExposure", ClampMin = "0.1", ClampMax = "20.0"))
+	float SpeedDown = 1.0f;
+};
